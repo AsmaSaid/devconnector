e01e988500dfa2998aeb6d495e4dda150167103c
asma <asma.said@waed.net>
2018-10-04 15:40:44 +0300

    prfile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-router.get('/test', (req, res) => res.json({
+// Load Profile Model
+const Profile = require("../../models/Profile");
+// Load User Model
+const User = require("../../models/User");
+
+// @Route GET api/profile/test
+// @desc test profile route
+// @access Public
+router.get("/test", (req, res) =>
+  res.json({
     message: "Profile works"
-}));
+  })
+);
+
+// @Route GET api/profile
+// @desc get current user profile
+// @access Public
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "there is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @Route POST api/profile
+// @desc Create user profile
+// @access private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.gethubusername)
+      profileFields.gethubusername = req.body.gethubusername;
+    // Skills split into array
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.splilt(",");
+    }
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+  }
+);
 
 module.exports = router;
